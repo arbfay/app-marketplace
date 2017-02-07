f1e1870504957c78bab0c1db3554cd8494761a8e
Arbai Fayçal <anticonarde>
2017-02-07 18:10:10 +0100

    Improvements : 1) First part of implementation of i18n with tap:i18n package (modification of DOM + creation of YML files)

--- client/startup.js
+++ client/startup.js
@@ -1,3 +1,20 @@
 Meteor.startup(function(){
   GoogleMaps.load({key:'AIzaSyCuNWnVv37wxgpCzzPK_tPJdMhbdys_Y64'});
+
+  Session.set("showLoadingIndicator", true);
+
+    TAPi18n.setLanguage(getUserLanguage())
+      .done(function () {
+        Session.set("showLoadingIndicator", false);
+      })
+      .fail(function (error_message) {
+        // Handle the situation
+        console.log(error_message);
+      });
 });
+
+getUserLanguage = function () {
+  // Put here the logic for determining the user language
+
+  return "fr";
+};
